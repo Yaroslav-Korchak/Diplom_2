d2fa4419858de6cf3c50a99bcefac454229564ae
Yaroslav-Korchak <yakorchak@mail.ru>
2023-12-25 20:06:38 +0300

    добавлены ссылки

---
+++ conftest.py
@@ -0,0 +1,32 @@
+import pytest
+import requests
+from data.data import Links
+import random
+import string
+
+
+@pytest.fixture
+def create_and_delete_user():
+    def generate_random_string(length):
+        letters = string.ascii_lowercase
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        return random_string
+
+    name = generate_random_string(10)
+    email = generate_random_string(10)+'@yandex.ru'
+    password = generate_random_string(10)
+
+    payload = {
+        "name": name,
+        "email": email,
+        "password": password
+    }
+
+    login_data = payload.copy()
+    login_data.pop("name")
+    response = requests.post(Links.register_url, data=payload)
+    access_token = response.json()["accessToken"]
+    yield response, login_data, access_token
+    requests.delete(Links.user_url, headers={'Authorization': access_token})
+
+
---
+++ data/data.py
@@ -0,0 +1,5 @@
+class Links:
+    register_url = 'https://stellarburgers.nomoreparties.site/api/auth/register'
+    user_url = 'https://stellarburgers.nomoreparties.site/api/auth/user'
+    login_url = 'https://stellarburgers.nomoreparties.site/api/auth/login'
+    order_url = 'https://stellarburgers.nomoreparties.site/api/orders'
